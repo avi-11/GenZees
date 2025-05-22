abc98c34a13243d576657c86659df4e64b5e46e6
avi-11 <singh.avi1109@gmail.com>
2025-05-22 22:59:31 +0530

    -- clerk auth added --

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
    <div>
     <h1>Hello home page</h1>
+    <SignedOut>
+              <SignInButton mode="modal" />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
    </div>
   );
 }
